184b7a244ddcec4c7aed659e83ac428bd45e92e4
jkloppenburg1 <jkloppenburg1>
2016-09-22 16:36:06 -0700

    misc

--- app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/flagquiz/QuizActivity.java
@@ -1,15 +1,23 @@
 package edu.orangecoastcollege.cs273.flagquiz;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+
+    // keys for reading data from Shared Preferences
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true; //for portrait mode
+    private boolean preferencesChanged = true; // did preferences change?
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,7 @@ public class QuizActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /** FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,27 +34,77 @@ public class QuizActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
+
+        // Set default preferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(
+                        preferencesChangeListener);
+
+        // determine screen size
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // if device is tablet, set phoneDevice to false
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+        {
+            phoneDevice = false; // Not a phone
+        }
+
+        //If on a phone, only portrait orientation
+        if (phoneDevice)
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
 
     @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        if (preferencesChanged)
+        {
+            //Default preferences have been set,
+            // now initialize QuizActivityFragment and start the quiz
+            QuizActivityFragment quizFragment = (QuizActivityFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateGuessRows(
+                    PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(
+                    PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+        }
+    }
+
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // get device's orientation
+        int orientation = getResources().getConfiguration().orientation;
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            // inflate the menu
+            getMenuInflater().inflate(R.menu.menu_quiz, menu);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
     }
+
+    //Add OnSharedPreferenceChangeListener()
 }
